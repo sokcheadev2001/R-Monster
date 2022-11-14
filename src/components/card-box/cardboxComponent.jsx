cf5591d5f3fa7fa645aee44d4f5dff3cabee964c
sokcheadev2001 <sokchealeang.dev@gmail.com>
2022-11-14 23:31:17 +0700

    latest

--- src/components/card-box/cardboxComponent.jsx
+++ src/components/card-box/cardboxComponent.jsx
@@ -1,19 +1,16 @@
-import React, { Component } from "react";
 import "./cardBox.styles.css";
-export class CardBox extends Component {
-  render() {
-    const { id, name, email } = this.props.monster;
-    return (
-      <div className="card-container">
-        <img
-          alt={`monster ${name}`}
-          src={`https://robohash.org/${id}/?set=set2&size=180x180`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+const CardBox = ({ monster }) => {
+  const { id, name, email } = monster;
+  return (
+    <div className="card-container">
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}/?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default CardBox;
